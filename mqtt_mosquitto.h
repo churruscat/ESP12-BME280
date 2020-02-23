57dab3e46439bffd50502a423e86d0a86abb7278
churruscat <luis.morras@gmail.com>
2020-02-23 11:12:34 +0100

    Update mqtt_mosquitto.h

--- mqtt_mosquitto.h
+++ mqtt_mosquitto.h
@@ -12,10 +12,10 @@
 #define ORG "canMorras"
 char* ssid;
 char* password;
-char ssid1[] = "MOVISTAR_CASA";
-char password1[] = "Vivan1los2Morras";
-char ssid2[] = "MOVISTAR_CASA";
-char password2[] = "Vivan1los2Morras";
+char ssid1[] = "ssid1";
+char password1[] = "Password_ssid1";
+char ssid2[] = "ssid2";
+char password2[] = "Password_ssid2";
 /*************************************************
  ** ----- Fin de Valores Personalizados ------- **
  * ***********************************************/
